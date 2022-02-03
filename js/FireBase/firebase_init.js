ae0c5256f8c38d2a3dc27501c4c66036c3bde5d3
Ttiki <clem64.combier@gmail.com>
2022-02-03 19:17:17 +0100

    Firebase & La bilbiothèque rework (W.I.P).

--- js/FireBase/firebase_init.js
+++ js/FireBase/firebase_init.js
@@ -1,4 +1,11 @@
-var firebaseConfig = {
+import { initializeApp } from 'firebase/app'
+import {
+    getFireStore,
+    collection
+} from 'firebase/firestore'
+
+//Firebase data
+const firebaseConfig = {
     apiKey: "AIzaSyCn5vwgBW6ioYfuZWHyKMMrVuNPjNfQn1g",
     authDomain: "ttiki-github-page.firebaseapp.com",
     databaseURL: "https://ttiki-github-page-default-rtdb.europe-west1.firebasedatabase.app",
@@ -6,7 +13,20 @@ var firebaseConfig = {
     storageBucket: "ttiki-github-page.appspot.com",
     messagingSenderId: "485299721979",
     appId: "1:485299721979:web:7c29ebfba3c86a8b01825e"
-}
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig)
-var firestoreDb = firebase.firestore()
+};
+
+//Init our app
+initializeApp(firebaseConfig)
+
+//Init services
+const firestoreDb = getFireStore()
+
+//collection ref
+const libraryRef = collection(firestoreDb, "library")
+
+//get collection data
+getDocs(libraryRef).then((snapshot) => {
+    console.log(snapshot.docs)
+}).catch((err) => {
+
+});
